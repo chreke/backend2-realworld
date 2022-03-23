8087e81fdcad0c55fd45358b4d15a53367d3324b
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-03-23 11:01:21 +0100

    test commit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.static("dist"));
 
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
+});´
+
+app.get("/test", (_req, res) => {
+  res.send("test");
 });
 
 app.listen(PORT, () => {
