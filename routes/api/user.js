fa2256dc6b853dc458ee103bfd95c8b2c39303ab
panosjapan7 <panosjapan@gmail.com>
2022-03-24 09:43:41 +0100

    created a fake response for api/user

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,18 @@ const { Router } = require("express");
 
 const route = Router();
 
-
+route.get("/", (req, res) => {
+    
+    // Checked if "http://localhost:3000/api/user" route establed. It did!
+    res.send({
+        "user": {
+            "email": "panos@panos.panos",
+            "token": "jwt.token.here",
+            "username": "panos",
+            "bio": "I am Panos",
+            "image": null,
+        }
+    })
+})
 
 module.exports = route
