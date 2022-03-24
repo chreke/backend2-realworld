0ac6261433777fb1457f1abf5a0550d071ab6758
sandro <akecakabro@gmail.com>
2022-03-24 14:53:46 +0100

    add user-login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const { use } = require("bcrypt/promises");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
@@ -12,7 +13,16 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10)
   }
   next()
-})
+});
+
+userSchema.statics.login=async function(email,password){
+  const user= await this.findOne({email:email}).select('+password');
+  if(user && await bcrypt.compare(password,user.password)){
+    return user
+  } else{
+    return null
+  }
+};
 
 const User = mongoose.model("User", userSchema)
 
