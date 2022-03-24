84d7e2e7c75fc08724b67b83fe6f2b9ac8eb84e6
Anna Rylander <annarylander@AirsomtlhorAnna.lan>
2022-03-24 11:36:41 +0100

    db-connection, bodyparser

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const express = require("express");
 const path = require("path");
-const passport = require("passport")
-const session = require("express-session")
-const { User } = require("./models/User")
+const passport = require("passport");
+const session = require("express-session");
+const { User } = require("./models/User");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 const PORT = 3000;
 
 app.use(express.static("dist"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.post("/register", (_req, res) => {
-  res.send("test");
+app.post("/register", async (_req, res) => {
+  const {email, password, username} = req.body
+  try {
+  
 });
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(session({
+app.use(
+  session({
     secret: "nemas",
     resave: false,
-    saveUninitialized: false
-  }));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.authenticate("session"));
 
 app.post("/login", (_req, res) => {
@@ -35,3 +42,21 @@ app.post("/login", (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
 });
+
+//establish connection to database
+mongoose.connect(
+  "mongodb+srv://annarylander:node2022@cluster0.fjx89.mongodb.net/realworld",
+  {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) return console.log("Error: ", err);
+    console.log(
+      "MongoDB Connection -- Ready state is:",
+      mongoose.connection.readyState
+    );
+  }
+);
