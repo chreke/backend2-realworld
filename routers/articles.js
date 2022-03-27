ce997a97958019ec424dcb0bfa290d01cbec1107
Hanna <hanna.tylna@gmail.com>
2022-03-27 22:03:17 +0200

    added articles output and their filter

--- routers/articles.js
+++ routers/articles.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const { Article } = require("../models/article")
 const { Tag } = require("../models/tag")
 const { filterUnique, lowerCase } = require("../utils")
+const slugify = require("slugify")
+const { User } = require("../models/user")
 
 const router = Router()
 
@@ -24,18 +26,55 @@ router.post("/", async (req, res) => {
 
   const uniqueTags = filterUnique(tagList).map(lowerCase)
   const tags = await saveTags(uniqueTags)
-
+  const slug = slugify(title)
   const article = new Article({
     title,
     description,
     body,
     tagList: tags,
     author: userId,
+    slug: slug,
   })
 
   const savedArticle = await article.save()
 
   res.json({ article: savedArticle })
+})
+
+router.get("/", async (req, res, next) => {
+  let query = {}
+  let limit = 10
+  let offset = 0
+
+  if (typeof req.query.limit !== "undefined") {
+    limit = req.query.limit
+  }
+  if (typeof req.query.offset !== "undefined") {
+    offset = req.query.offset
+  }
+  if (typeof req.query.tag !== "undefined") {
+    const tag = await Tag.findOne({ name: req.query.tag }) // .find
+    query.tagList = tag._id
+  }
+
+  const author = await User.findOne({ username: req.query.author })
+  if (author) {
+    query.author = author._id
+  }
+  //console.log(author) // fails test "Articles by Author"
+
+  const articles = await Article.find(query)
+    .limit(Number(limit))
+    .skip(Number(offset))
+    .sort({ createdAt: -1 })
+    .populate("author")
+    .exec()
+  const articlesCount = await Article.count(query).exec()
+
+  res.json({
+    articles: articles,
+    articlesCount: articlesCount,
+  })
 })
 
 module.exports = router
