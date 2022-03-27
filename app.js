15d6a5dbc20d159f9080b9aebe586d88ba0548c9
Mattias Söderberg <mattiassoderberg@MacBook-Pro-som-tillhor-Mattias.local>
2022-03-27 14:43:11 +0200

    Add mongoose connect

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose")
 
 const app = express()
 const PORT = 3000;
 
 
-app.use(express.static("dist"));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.use(express.static("dist"));
 
-app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+    app.get("/", (_req, res) => {
+      res.sendFile(path.join(__dirname, "dist", "index.html"));
+    });
 
-app.listen(PORT, () => {
-  console.log(`Started Express server on port ${PORT}`);
-});
-
+    app.listen(PORT, () => {
+      console.log(`Started Express server on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo connection error: ", err)
+  })
