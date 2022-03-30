35b93e8c1c394e3f227909280c65b4a6291671a0
Theo-Selin <theo.selin@hotmail.com>
2022-03-30 10:06:40 +0200

    MongoDB Connection done

---
+++ database.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config()
+console.log(process.env)
+
+class Database {
+
+    constructor() {
+        this.connect()
+    }
+
+    connect() {
+        mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log("Connected successfully to database")
+        })
+        .catch(() => {
+            console.log("Error" + err)
+        })
+    }
+}
+
+module.exports = new Database()
