98adcae34ea3a89c364d445813bfa9f892a700f7
Mattias Söderberg <mattes.email@gmail.com>
2022-03-30 20:59:30 +0200

    Add user post route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,9 @@
 const express = require("express")
+const userRouter = express.Router()
+const { userController } = require("../controllers/userController")
+
+userRouter.post("/", (req, res) => {
+    userController.userCreate(req)
+})
+
+module.exports = userRouter
