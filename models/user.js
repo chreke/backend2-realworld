e44d5f5d53dfdddea907e8c9154704aaec026db2
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-03-31 13:42:41 +0200

    Added user model, register endpoint

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  token: { type: String },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.modifiedPaths().includes("password")) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
+});
+
+userSchema.statics.login = async function (username, password) {
+  const user = await this.findOne({ username });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return user;
+  }
+  return null;
+};
+const User = mongoose.model("User", userSchema);
+
+exports.User = User;
