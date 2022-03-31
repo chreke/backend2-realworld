8555f7e6790ed01604a662643c6afe26a9894d7f
PatrikVahlen <91195238+PatrikVahlen@users.noreply.github.com>
2022-03-31 17:00:24 +0200

    Updated model

--- models/article.js
+++ models/article.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose")
 
 const articleSchema = new mongoose.Schema(
     {
-        title: {type: String, required: true},
-        description: {type: String, required: true},
-        body: {type: String, required: true},
-        tagList: {type: Array, 'default': []}
+        slug: { type: String },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        body: { type: String, required: true },
+        tagList: { type: Array, 'default': [] },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+        favorited: { type: Boolean, default: false },
+        favoritesCount: { type: String },
+        author: { type: mongoose.Schema.ObjectId, ref: "User" }
     })
 
 const Article = mongoose.model("Article", articleSchema);
