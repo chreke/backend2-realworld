a87fc8f1b06484d47f2717f672a66627e4e0aaba
imakero <45946863+imakero@users.noreply.github.com>
2022-03-31 11:06:44 +0200

    add favorites feature

--- models/article.js
+++ models/article.js
@@ -8,9 +8,14 @@ const articleSchema = new mongoose.Schema(
     slug: { type: String, required: true, lowercase: true },
     tagList: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    favoritedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 )
+
+articleSchema.virtual("favoritesCount").get(function () {
+  return this.favoritedBy.length
+})
 
 const Article = mongoose.model("Article", articleSchema)
 
