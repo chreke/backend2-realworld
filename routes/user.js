c9e30083c3ae8df2b48f6cf26fc21d2e1877b1c9
john sjödén <johnsjoden95@gmail.com>
2022-04-06 11:33:56 +0200

    user routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { user_login, user_update, verify } = require("../controllers/userController");
+const { user_login, user_update, verify, create_user, getUser } = require("../controllers/userController");
 const router = express.Router()
-router.post("/users/login", user_login)
-router.put("/user", verify, user_update)
-
+router.post("/login", user_login)
+router.put("/", verify, user_update)
+router.post("/", create_user)
+router.get("/", getUser)
 module.exports = router
