81efad0d6dba0b8e25adae83fe65ad4f60b3db2f
john sjödén <johnsjoden95@gmail.com>
2022-04-06 11:33:41 +0200

    routes for articles

---
+++ routes/article.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const { article_Create, get_Article, find_Article_Slug, updateArticle, favorite_Article, unFavorite_Article } = require("../controllers/articleController")
+
+const router = express.Router()
+
+router.post("/", article_Create)
+router.get("/", get_Article)
+/* router.delete("/", delete_Article) */
+router.get("/:slug", find_Article_Slug)
+router.put("/:slug", updateArticle)
+router.post("/:slug/favorite", favorite_Article)
+router.delete("/:slug/favorite", unFavorite_Article)
+module.exports = router 
