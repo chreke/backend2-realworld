8df3b635d9f7f2a7117399c4e5fac4bd916f2937
Mattias Söderberg <mattes.email@gmail.com>
2022-04-06 14:27:37 +0200

    Add user route and refactoring

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors")
 const mongoose = require("mongoose")
-const userRouter = require("./routes/userRoutes");
+const usersRouter = require("./routes/users");
 const { authUser } = require("./controllers/auth");
+const userRouter = require("./routes/user")
 
 const app = express()
 const PORT = 3000;
 
 
-mongoose.connect('mongodb://localhost:27017/real-world')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.use(express.json())
     app.use(cors())
     app.use(authUser)
     app.use(express.static("dist"));
-    app.use("/api/users", userRouter)
+    app.use("/api/users", usersRouter)
+    app.use("/api/user", userRouter)
     
 
     app.get("/", (_req, res) => {
