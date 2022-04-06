8e4b5bf65a1e398d003c496b409fe14a41552df8
Mattias Söderberg <mattes.email@gmail.com>
2022-04-06 10:27:14 +0200

    Clean up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 const express = require("express");
 const path = require("path");
+const cors = require("cors")
 const mongoose = require("mongoose")
 const userRouter = require("./routes/userRoutes");
 const { authUser } = require("./controllers/auth");
@@ -13,6 +14,7 @@ const PORT = 3000;
 mongoose.connect('mongodb://localhost:27017/real-world')
   .then(() => {
     app.use(express.json())
+    app.use(cors())
     app.use(authUser)
     app.use(express.static("dist"));
     app.use("/api/users", userRouter)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,45 +5,34 @@ const cors = require("cors");
 const User = require("../models/User");
 const { userController } = require("../controllers/userController");
 const {
-  checkEmptyFields,
-  validateUsernameAndEmail,
+    checkEmptyFields,
+    validateUsernameAndEmail,
 } = require("../middlewares/validation");
-//const jwt = require("jsonwebtoken");
-//const { authUser } = require("../controllers/auth");
-const {login} = require("../models/User")
-const { authUser, generateToken}= require("../controllers/auth")
-
-
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(authUser)
+const { login } = require("../models/User")
+const { authUser, generateToken } = require("../controllers/auth")
 
 userRouter.post("/", checkEmptyFields, validateUsernameAndEmail, (req, res) => {
 
-  userController.userCreate(req, res);
-  
+    userController.userCreate(req, res);
+
 
 });
 
-userRouter.post("/login", async (req,res) =>{
-
-  
+userRouter.post("/login", async (req, res) => {
     const { email, password } = req.body.user;
     const user = await login(email, password);
     //console.log(user)
     if (user) {
-      const token = await generateToken(user);
-      console.log(user)
-      res.json({ user: {token:token, email:user.email, username:user.username} });
+        const token = await generateToken(user);
+        user.token = token
+        await user.save()
+        res.json({ user });
     } else {
-      res.sendStatus(401);
-      
+        res.sendStatus(401);
     }
 })
 
-userRouter.get("/user", authUser, (req, res)=>{
+userRouter.get("/user", authUser, (req, res) => {
     const { username } = req.user;
     res.json({ username });
 })
