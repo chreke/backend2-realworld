0c891caa1b672541c3eb302f0eafb5196224db29
karl dollmayer <karldollmayer@gmail.com>
2022-04-07 15:20:27 +0200

    update /author

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-const {createArticle, renderArticles, renderArticlesAuthor} = require("../controllers/articleController");
+const {createArticle, renderArticles} = require("../controllers/articleController");
 const { Article } = require('../models/Article');
 const { User } = require('../models/User');
 
-router.get('/api/articles', renderArticlesAuthor)
+
 router.get('/api/articles', renderArticles)
 router.get('/api/articles/feed', renderArticles)
 router.post('/api/articles', createArticle);
