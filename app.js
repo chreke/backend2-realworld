52806ab8e6c6ecb84081ebfdfb61a9cd6a67599b
Kriksy <viktoria.toldi@gmail.com>
2022-04-07 16:22:58 +0200

    Add JWT token check and add user to request (req.user)

--- app.js
+++ app.js
@@ -3,28 +3,59 @@ const app = express()
 const PORT = 3000;
 const path = require("path");
 
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "34567hthgfkjfhgsfesghfgjhmcvcgn";
+
 // Connection to MongoDB Atlas
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://panos:123@realworld-gruppuppgift.qxob6.mongodb.net/realworld-gruppuppgift?retryWrites=true&w=majority")
-.then(() => {
-  console.log("Connection to MongoDB successful");
-})
-.catch((err) => {
-  console.log("Connection to MongoDB error " + err);
-})
+  .then(() => {
+    console.log("Connection to MongoDB successful");
+  })
+  .catch((err) => {
+    console.log("Connection to MongoDB error " + err);
+  })
 
 // For POST requests
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
 app.use("/", require("./routes/api"));
-
 
 app.use(express.static("dist"));
 
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// "requireLogin" behöver lämna hos Create Article och Update Article for check token
+// req.user exists if JWT Token Check == OK 
+const requireLogin = (req, res, next) => {
+  if (req.user) {
+    next()
+  } else {
+    res.sendStatus(401); // 401 - Unauthorized
+  }
+}
+
+// -- JWT Token Check
+app.use((req, _res, next) => {
+  const authHeader = req.header("Authorization");
+  console.log("req.header:", req.header)
+  console.log("authHeader: ", authHeader)
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log("Token:", token);
+    if (token) {
+      req.user = jwt.verify(token, JWT_SECRET);
+    }
+
+  }
+  console.log("User: ", req.user)
+  next();
+
 });
 
 app.listen(PORT, () => {
