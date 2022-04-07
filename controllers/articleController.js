71009883e882e8831e11d91cf5a12f55ab35a063
yihu55 <veronicayi@msn.com>
2022-04-07 14:07:41 +0200

    getsinglearticlebyslug

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,47 +1,47 @@
-const { Article } = require("../models/Article")
-
-
+const asyncHandler = require('express-async-handler');
+const { Article } = require('../models/Article');
 
 const createArticle = async (req, res) => {
-    const { title, description, body, tagList } = req.body.article;
-    
+  const { title, description, body, tagList } = req.body.article;
 
-    const entry = new Article({title, description, body, tagList, author: req.user.userId } )
-    await entry.save()
+  const entry = new Article({
+    title,
+    description,
+    body,
+    tagList,
+    author: req.user.userId,
+  });
+  await entry.save();
 
-    res.json({
-        article: {
-            title: entry.title,
-            description: entry.description,
-            body: entry.body,
-            tagList: entry.tagList,
-            createdAt: entry.createdAt,
-            slug: entry.slug,
-            updatedAt: entry.updatedAt,
-            author: entry.author,
-            favorited: entry.favorited,
-            favoritesCount: entry.favoritesCount
-        
-        }
-    })
-
-}
-
-
+  res.json({
+    article: {
+      title: entry.title,
+      description: entry.description,
+      body: entry.body,
+      tagList: entry.tagList,
+      createdAt: entry.createdAt,
+      slug: entry.slug,
+      updatedAt: entry.updatedAt,
+      author: entry.author,
+      favorited: entry.favorited,
+      favoritesCount: entry.favoritesCount,
+    },
+  });
+};
 
 const renderArticles = async (req, res) => {
+  try {
+    const articlesCount = await Article.find().count();
+    const articles = await Article.find().sort('-createdAt').exec();
+    res.json({ articles, articlesCount });
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
 
-    try {
-        const articlesCount = await Article.find().count()
-        const articles = await Article.find().sort('-createdAt').exec()
-        res.json( { articles, articlesCount })
-       
-        
-    } catch (err) {
-        res.json({message: err})
-    }
-    
-}
-
-
-module.exports =  { createArticle, renderArticles }
+const getSingleArticleBySlug = asyncHandler(async (req, res) => {
+  const slug = req.params.slug;
+  const article = await Article.findOne({ slug }).populate('author').exec();
+  res.json({ article });
+});
+module.exports = { createArticle, renderArticles, getSingleArticleBySlug };
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+  createArticle,
+  renderArticles,
+  getSingleArticleBySlug,
+} = require('../controllers/articleController');
 
-const {createArticle, renderArticles} = require("../controllers/articleController")
-
-router.get('/api/articles', renderArticles)
+router.get('/api/articles', renderArticles);
 router.post('/api/articles', createArticle);
+router.get('/api/articles/:slug', getSingleArticleBySlug);
 
 module.exports = router;
