dd3b89bcabca70c659860a9f7f466d51da533f2f
Mattias Söderberg <mattes.email@gmail.com>
2022-04-10 18:21:03 +0200

    Change author to String instead of ObjectId

--- models/Article.js
+++ models/Article.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose")
 
 const articleSchema = mongoose.Schema({
     title: { type: String, required: true },
-    slug: { type: String, default: ""},
+    slug: { type: String, default: "" },
     description: { type: String, required: true },
     body: { type: String, required: true },
     tagList: [String],
-    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    author: String,
     favorited: { type: Boolean, default: false },
     favoritesCount: { type: Number, default: 0 }
 }, { timestamps: true })
@@ -17,4 +17,8 @@ const createArticle = async (articleData) => {
     return await Article.create(articleData)
 }
 
-module.exports = { createArticle }
+const getAllArticles = async (query) => {
+    return await Article.find(query)
+}
+
+module.exports = { createArticle, getAllArticles }
