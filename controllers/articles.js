1a16286ed5895139b4d9393791892507a2a6d47b
Mattias Söderberg <mattes.email@gmail.com>
2022-04-10 18:22:03 +0200

    Add get article with filter on author and tag

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,4 @@
-const { createArticle } = require("../models/Article")
+const { createArticle, getAllArticles } = require("../models/Article")
 
 const createNewArticle = async (req, res) => {
     const { title, description, body, tagList } = req.body.article
@@ -7,14 +7,34 @@ const createNewArticle = async (req, res) => {
         description,
         body,
         tagList,
-        author: req.user.userId
+        author: req.user.username
     }
     const article = await createArticle(articleData)
     if (article) {
-        res.json({article})
+        res.json({ article })
     } else {
         res.sendStatus(400)
     }
 }
 
-module.exports = { createNewArticle }
+const getArticles = async (req, res) => {
+    const queryParam = Object.keys(req.query)[0]
+    let articles = []
+    if (queryParam === "author") {
+        articles = await getAllArticles(req.query)
+    } else if (queryParam === "tag") {
+        articles = await getAllArticles({ tagList: { $in: Object.values(req.query)[0] } })
+    } else {
+        articles = await getAllArticles({})
+    }
+
+    const articlesCount = articles.length
+
+    if (articles) {
+        res.json({ articles, articlesCount })
+    } else {
+        res.sendStatus(400)
+    }
+}
+
+module.exports = { createNewArticle, getArticles }
