0523878d775d015e3b8e76dc09fb2e9601b97073
panosjapan7 <panosjapan@gmail.com>
2022-04-11 11:20:25 +0200

    Fixed Favorite/Unvfavorite, deleted unused code

--- routes/api/articles/index.js
+++ routes/api/articles/index.js
@@ -18,47 +18,7 @@ route.get("/", async (req, res) => {
     // Add articlesCount field and value to articles
     articles = { articles }, articles.articlesCount = articlesCount;
 
-    // console.log(articles)
     res.send(articles)
-
-    // Dummy Data
-    // res.send({
-    //     "articles": [{
-    //         "slug": "how-to-train-your-dragon",
-    //         "title": "How to train your dragon",
-    //         "description": "this is the description",
-    //         "body": "This is the body",
-    //         "tagList": ["dragons", "training"],
-    //         "createdAt": new Date(),
-    //         "updatedAt": new Date(),
-    //         "favorited": false,
-    //         "favoritesCount": 0,
-    //         "author": {
-    //             "username": "panos",
-    //             "bio": "Hello there",
-    //             "image": "https://st3.depositphotos.com/26608456/31707/i/450/depositphotos_317074692-stock-photo-cute-white-lama-with-black.jpg",
-    //             "following": false
-    //         }
-    //     },
-    //     {
-    //         "slug": "how-to-train-your-dragon-2",
-    //         "title": "How to train your dragon 2",
-    //         "description": "this is the description",
-    //         "body": "This is the body",
-    //         "tagList": ["dragons", "training"],
-    //         "createdAt": new Date(),
-    //         "updatedAt": new Date(),
-    //         "favorited": false,
-    //         "favoritesCount": 0,
-    //         "author": {
-    //             "username": "panos",
-    //             "bio": "Hello there",
-    //             "image": "https://st3.depositphotos.com/26608456/31707/i/450/depositphotos_317074692-stock-photo-cute-white-lama-with-black.jpg",
-    //             "following": false
-    //         }
-    //     }],
-    //     "articlesCount": 2
-    // })
 })
 
 
@@ -67,60 +27,6 @@ route.get("/:slug", async (req, res) => {
 
     var article = await Article.findOne({ slug: req.params.slug });
     res.send({ article })
-    console.log("article:")
-    console.log(article)
-
-    // res.send({
-    //     "article": {
-    //         "slug": req.params.slug,
-    //         "title": "How to train your dragon",
-    //         "description": "this is the description",
-    //         "body": "This is the body",
-    //         "tagList": ["dragons", "training"],
-    //         "createdAt": new Date(),
-    //         "updatedAt": new Date(),
-    //         "favorited": false,
-    //         "favoritesCount": 0,
-    //         "author": {
-    //             "username": "panos",
-    //             "bio": "Hello there",
-    //             "image": "https://st3.depositphotos.com/26608456/31707/i/450/depositphotos_317074692-stock-photo-cute-white-lama-with-black.jpg",
-    //             "following": false
-    //         }
-    //     }
-    // })
-})
-
-// Get comments on article
-route.get("/:slug/comments", (req, res) => {
-    res.send({
-        "comments": [
-            {
-                "id": 1,
-                "createdAt": new Date(),
-                "updatedAt": new Date(),
-                "body": "This is the 1st comment's body",
-                "author": {
-                    "username": "panos",
-                    "bio": "Hello there",
-                    "image": "https://st3.depositphotos.com/26608456/31707/i/450/depositphotos_317074692-stock-photo-cute-white-lama-with-black.jpg",
-                    "following": false
-                }
-            },
-            {
-                "id": 2,
-                "createdAt": new Date(),
-                "updatedAt": new Date(),
-                "body": "This is the 2nd comment's body",
-                "author": {
-                    "username": "mary",
-                    "bio": "Hello, I am Mary",
-                    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXZCwR5JBTDBHaq9YzKXrxALwYzzxlSazJ1A&usqp=CAU",
-                    "following": false
-                }
-            }
-        ]
-    })
 })
 
 
@@ -150,13 +56,9 @@ route.post("/", requireLogin, async (req, res) => {
 // UPDATE AN ARTICLE
 // It works but I can't identify the article I'm updating without being able to know which user is executing the PUT method
 route.put("/:article", async (req, res) => {
-    console.log("Update article PUT:");
-    console.log("req")
-    console.log(req.body)
 
     let slug = req.params.article
     console.log("slug:", slug)
-
 
     // https://mongoosejs.com/docs/tutorials/findoneandupdate.html
     const filter = { slug };
@@ -171,9 +73,7 @@ route.put("/:article", async (req, res) => {
 // FAVORITE ARTICLE
 route.post("/:article/favorite", async (req,res) => {
 
-    console.log("Favorite article POST:");
-
-    var article = await Article.findOne({slug});
+    var article = await Article.findOne({});
     
     // For testing purposes
         // var articleId = "62514a9182197faaa9d4b03a"; 
@@ -193,9 +93,7 @@ route.post("/:article/favorite", async (req,res) => {
 // UNFAVORITE ARTICLE
 route.delete("/:article/favorite", async (req,res) => {
 
-    console.log("Unfavorite article DELETE:");
-
-    var article = await Article.findOne({slug});
+    var article = await Article.findOne({});
 
     // For testing purposes
         // var articleId = "62514a9182197faaa9d4b03a"; 
