e1b6ee1173a7ac5bf1c65d63143198a96b27bebc
john sjödén <johnsjoden95@gmail.com>
2022-04-13 20:15:36 +0200

    send jwt expired if jwt is expired or wrong atm.

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,16 @@ exports.auth_MiddleWare = function(req, res, next) {
     if(auth){
         const token = auth.split(" ")[1]
         const JWT_SECRET = process.env.JWT_SECRET
-        req.user = jwt.verify(token, JWT_SECRET)
+        jwt.verify(token, JWT_SECRET, function(err){
+            if(err){
+                res.send("JWT expired. Login")
+            }else {
+               req.user = jwt.verify(token, JWT_SECRET)
+               next()
+            }
+        }) 
+        
+    }else {
+        next()
     }
-    next()
 }
